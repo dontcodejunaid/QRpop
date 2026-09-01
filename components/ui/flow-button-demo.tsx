000689673334d8ad71bb2154677b385658954deb
dontcodejunaid <baigjunaid187@gmail.com>
2026-09-01 17:10:21 +0530

    Integrate FlowButton component into /components/ui and refine smooth hover animation

---
+++ components/ui/flow-button-demo.tsx
@@ -0,0 +1,13 @@
+'use client';
+import React from 'react';
+import { FlowButton } from './flow-button';
+
+export function FlowButtonDemo() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-100 p-4">
+      <FlowButton text="Flow Button" />
+    </div>
+  );
+}
+
+export default FlowButtonDemo;
